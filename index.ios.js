2b5e85396c4f78d73db1b162fc1670eb5da0386b
Nikolay Nemshilov <nemshilov@gmail.com>
2015-10-06 16:02:11 +1100

    Initializing the app

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+import React from "react-native";
+import App from "./src/app.ios.js";
+
+React.AppRegistry.registerComponent('MeowTVApp', () => App);
---
+++ src/app.ios.js
@@ -0,0 +1,22 @@
+import React from "react-native";
+import styles from "./styles";
+let {Text, View} = React;
+
+export default class MeowTVApp extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
